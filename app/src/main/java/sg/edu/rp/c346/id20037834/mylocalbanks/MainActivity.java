a86191c00ffdb14b6da7ce2a6eaeba329812c2b4
20037834 <20037834@myrp.edu.sg>
2021-05-28 14:50:26 +0800

    Added context menu to the buttons to let them choose website or contact the bank and linked the codes to the UI.

--- app/src/main/java/sg/edu/rp/c346/id20037834/mylocalbanks/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20037834/mylocalbanks/MainActivity.java
@@ -3,12 +3,36 @@ package sg.edu.rp.c346.id20037834.mylocalbanks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnDBS;
+    Button btnOCBC;
+    Button btnUOB;
+    String btnChosen = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnDBS = findViewById(R.id.btnDBS);
+        btnOCBC = findViewById(R.id.btnOCBC);
+        btnUOB = findViewById(R.id.btnUOB);
+
+        registerForContextMenu(btnDBS);
+        registerForContextMenu(btnOCBC);
+        registerForContextMenu(btnUOB);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0,0,0,"Website");
+        menu.add(0,1,1,"Contact the bank");
     }
 }
